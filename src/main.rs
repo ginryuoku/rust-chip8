2ee21c4ba23fe95e35c7d8565e9183413c546428
Michelle Darcy <michelle.darcy@pcc.edu>
2018-12-05 16:31:16 -0800

    Commit working sleketon

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::Read;
+use crate::chip8::Chip8;
 
 mod ram;
 mod chip8;
@@ -8,4 +9,7 @@ fn main() {
     let mut file = File::open("data/chip8/INVADERS").unwrap();
     let mut data = Vec::<u8>::new();
     file.read_to_end(&mut data);
+
+    let mut chip8 = Chip8::new();
+    chip8.load_rom(&data);
 }
--- src/ram.rs
+++ src/ram.rs
@@ -1,19 +1,19 @@
-struct Ram {
-    mem: [u8, 4096];
+pub struct Ram {
+    mem: [u8; 4096],
 }
 
 impl Ram {
-    fn new() -> Ram {
+    pub fn new() -> Ram {
         Ram {
-            mem: [0, 4096];
+            mem: [0; 4096]
         }
     }
 
-    fn write_byte(&mut self, address: u16, value: u8 ) {
+    pub fn write_byte(&mut self, address: u16, value: u8 ) {
 
     }
 
-    fn read_byte(&mut self, address: u16, value: u8 ) {
+    pub fn read_byte(&mut self, address: u16, value: u8 ) {
         
     }
 }
